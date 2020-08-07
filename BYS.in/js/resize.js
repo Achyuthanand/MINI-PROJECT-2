79ec2722476c158e2dfd1346c5653f6e69db0616
Achyuthanand <44645795+Achyuthanand@users.noreply.github.com>
2020-08-07 11:25:20 +0530

    Update resize.js

--- BYS.in/js/resize.js
+++ BYS.in/js/resize.js
@@ -1,27 +1,8 @@
-function myFunction2(y) {
-  if (y.matches) { 
-    
-    
-    alert("This website is designed only for Laptops/Desktops");
-  } 
-
-
-}
-
-var y = window.matchMedia("(max-width: 800px)");
-
-myFunction2(y);
-y.addListener(myFunction2); 
-
-
-
-
-
 function myFunction(x) {
   if (x.matches) { 
     
     
-    alert("Make sure that your browser is full screen");
+     alert("This website is designed only for Laptops/Desktops\nand\nMake sure that your browser is full screen");
   } 
 
 
